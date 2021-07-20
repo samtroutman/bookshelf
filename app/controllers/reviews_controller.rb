2da5c8a3e37f281d68d8fdd1565cf8a2a68dd882
samtroutman <troutm16@gmail.com>
2021-07-19 20:55:17 -0400

    add new review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,19 @@
 class ReviewsController < ApplicationController
+
+    def new
+        if @book = Book.find_by_id(params[:book_id])
+            @review = @book.reviews.build
+        else
+            @review = Review.new
+        end
+    end
+
+    def index
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:book_id, :title :content, :rating)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,11 @@ Rails.application.routes.draw do
 
   resources :reviews
   resources :genres
-  resources :books
+
+  resources :books do 
+    resources :reviews, only: [:new, index]
+  end
+  
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
