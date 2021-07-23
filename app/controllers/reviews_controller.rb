4f9988f53f415cf63102ec3c03212b3837ee77bd
samtroutman <troutm16@gmail.com>
2021-07-22 23:20:00 -0400

    fix review edit and controller, add helper

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,6 +13,8 @@ class ReviewsController < ApplicationController
 
     def create
         @review = current_user.reviews.build(review_params)
+        book = Book.find(params[:review][:book_id])
+
         if @review.save
             redirect_to review_path(@review)
         else
@@ -32,6 +34,26 @@ class ReviewsController < ApplicationController
         end
     end
 
+    def edit
+        if params[:book_id]
+            book = Book.find_by_id(params[:book_id])
+            if book.nil?
+                redirect_to book_path, alert: "Not found"
+            else
+                @review = book.posts.find_by(id: params[:id])
+                redirect_to book_review_path(book), alert: "not found" if @review.nil?
+            end
+        else
+            @review = Review.find(params[:id])
+        end
+    end
+
+    def update
+        @review = Review.find(params[:id])
+        @review.update(review_params)
+        redirect_to review_path(@review)
+    end
+
     def destroy
         @review.destroy
         redirect_to review_path
@@ -40,7 +62,7 @@ class ReviewsController < ApplicationController
     private
 
     def review_params
-        params.require(:review).permit(:book_id, :rating, :title, :content)
+        params.require(:review).permit(:book_id, :rating, :title, :content, :user_id)
     end
 
     def set_review
--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,9 @@
 module ReviewsHelper
+    def book_id_field(review)
+        if review.book.nil?
+            select_tag "review[book_id]", options_from_collection_for_select(Book.alpha, :id, :title)
+        else
+            hidden_field_tag "review[book_id", review.book_id
+        end
+    end
 end
