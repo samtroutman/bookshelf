3930aa9a6ef51850e8baa96814168cd32cfcd4d6
samtroutman <troutm16@gmail.com>
2021-07-23 22:11:32 -0400

    fix errors on user signup and book show

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,6 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :index, :edit]
   end
 
-  resources :users, only: [:show]
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
