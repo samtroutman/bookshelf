e23fa617c00ae93e9ae806a7106c8b4d3e2d3183
samtroutman <troutm16@gmail.com>
2021-07-23 22:48:19 -0400

    fix omniauth routes and login

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,11 @@ class User < ApplicationRecord
 
     has_secure_password
 
-
+    def self.from_omniauth(response)
+        User.find_or_create_by(uid: response[:uid], provider: response[:provider]) do |u|
+            u.username = response[:info][:name]
+            u.password = SecureRandom.hex(15)
+        end
+    end
 
 end
